eae5766bbc06ae18be87b50cf0d40276a3f150e6
Zac Xu <zacfirst@gmail.com>
2021-02-24 00:43:09 +1100

    feat: refactor to use promise

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // import AgoraRTC from "agora-rtc-sdk-ng"
 
-// const client = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
-
 var rtc = {
   // For the local client.
   client: null,
@@ -11,31 +9,48 @@ var rtc = {
 
 var options = {
   // Pass your app ID here.
-  appId: null,
+  appId: "",
   // Set the channel name.
-  channel: "demo_channel_name",
+  channel: "",
   // Pass a token if your project enables the App Certificate.
-  token: null,
+  token: "",
 };
 
-async function startBasicCall() {
+function startBasicCall() {
   /**
    * Put the following code snippets here.
    */
   rtc.client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
-  rtc.client.on("user-published", async (user, mediaType) => {
-    // Subscribe to a remote user.
-    await rtc.client.subscribe(user, mediaType);
-    console.log("subscribe success");
+  rtc.client
+    .join(options.appId, options.channel, options.token, null)
+    .then(uid => {
+      document.getElementById('message').innerHTML += uid;
+      // Create an audio track from the audio sampled by a microphone.
+      AgoraRTC
+        .createMicrophoneAudioTrack()
+        .then(localAudioTrack => {
+          rtc.localAudioTrack = localAudioTrack;
+          rtc.client
+            .publish([localAudioTrack])
+            .then(() => document.getElementById('message').innerHTML += "<br>publish success!");
+        })
+    });
 
-    // If the subscribed track is audio.
-    if (mediaType === "audio") {
-      // Get `RemoteAudioTrack` in the `user` object.
-      const remoteAudioTrack = user.audioTrack;
-      // Play the audio track. No need to pass any DOM element.
-      remoteAudioTrack.play();
-    }
+  rtc.client.on("user-published", (user, mediaType) => {
+    // Subscribe to a remote user.
+    rtc.client
+      .subscribe(user, mediaType)
+      .then(() => {
+        document.getElementById('message').innerHTML += "<br>subscribe success"
+        // If the subscribed track is audio.
+        if (mediaType === "audio") {
+          // Get `RemoteAudioTrack` in the `user` object.
+          const remoteAudioTrack = user.audioTrack;
+          // Play the audio track. No need to pass any DOM element.
+          remoteAudioTrack.play();
+        }
+      });
   });
 
   rtc.client.on("user-unpublished", user => {
@@ -44,21 +59,14 @@ async function startBasicCall() {
     // Destroy the container.
     playerContainer.remove();
   });
-
-  // Create an audio track from the audio sampled by a microphone.
-  rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-  // Publish the local audio track to the channel.
-  await rtc.client.publish([rtc.localAudioTrack]);
-
-  console.log("publish success!");
 }
 
-async function leaveCall() {
+function leaveCall() {
   // Destroy the local audio and track.
   rtc.localAudioTrack.close();
 
   // Leave the channel.
-  await rtc.client.leave();
+  rtc.client.leave();
 }
 
 startBasicCall();
